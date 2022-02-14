a8ef4d743f3631e83253152ded40f9c30781d1c8
Davide Di Marco <davide.dimarco@marconirovereto.it>
2022-02-14 09:35:42 +0100

    add base javascript

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // CHOSEN API: https://networkcalc.com/api/docs
 
+const addressInput = document.querySelector("#addressInput");
+const submitBtn    = document.querySelector("#submitBtn");
+const outputDiv    = document.querySelector("#calculatedData");
 
+submitBtn.addEventListener("click", processData);
+
+function processData (event) {
+    event.preventDefault();
+    console.log("ciao");
+}
