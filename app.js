42c7cc7e4874b6eb89c121e389cec342ee8acb7b
Davide <davide.dimarco@marconirovereto.it>
2022-02-21 22:36:17 +0100

    changed api, added base fetch, add base for data extraction

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-// CHOSEN API: https://networkcalc.com/api/docs
+const baseURL = "https://api.hackertarget.com/subnetcalc/";
 
+// selectors
 const addressInput = document.querySelector("#addressInput");
 const submitBtn    = document.querySelector("#submitBtn");
 const outputDiv    = document.querySelector("#calculatedData");
+const binarySw     = document.querySelector("#binarySw");
 
+// listeners
 submitBtn.addEventListener("click", processData);
 
-function processData (event) {
+/* example response
+Address       = 192.168.1.0
+Network       = 192.168.1.0 / 24
+Netmask       = 255.255.255.0
+Broadcast     = 192.168.1.255
+Wildcard Mask = 0.0.0.255
+Hosts Bits    = 8
+Max. Hosts    = 254   (2^8 - 2)
+Host Range    = { 192.168.1.1 - 192.168.1.254 }
+*/
+function extractData (data) {
+    
+}
+
+async function processData (event) {
     event.preventDefault();
     outputDiv.innerHTML = "";
     
     if (addressInput.value.trim() != "") {
         
+        for (const line of addressInput.value.trim().split("\n")) {
+            let [start, ...masks] = addressInput.value.trim().split("/");
+
+            for (const mask of masks) {
+                rawData = await fetch(baseURL + "?q=" + start + "/" + mask).then(res=>res.text());
+
+                console.log(rawData);
+            }
+        }
     } else {
         outputDiv.innerHTML = "<h3>No input data</h3>";
     }
