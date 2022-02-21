cbb64c23b248ed9e4cec2d12525a918f8eb80047
Davide Di Marco <davide.dimarco@marconirovereto.it>
2022-02-21 09:25:48 +0100

    tried to add base logic

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 // CHOSEN API: https://networkcalc.com/api/docs
+const baseURL = "https://networkcalc.com/api/ip/";
+const id      = "hbsKqE4wayhlhgfW";
+const secret  = "5QJG9AZJbl9Qcv15BoMWodMvtU34oXB4gy8VtMr1Oh84yEtNTLcNEWvuhcFC2CPy";
 
 const addressInput = document.querySelector("#addressInput");
 const submitBtn    = document.querySelector("#submitBtn");
 const outputDiv    = document.querySelector("#calculatedData");
 
+// switches
+const binarySw     = document.querySelector("#binarySw");
+
+// add event listener
 submitBtn.addEventListener("click", processData);
+
+
 
 function processData (event) {
     event.preventDefault();
     outputDiv.innerHTML = "";
     
     if (addressInput.value.trim() != "") {
+        raw  = addressInput.value.trim();
+        data = []
         
+        // cycle trhough subnet entries
+        for (const entry of raw.split("\n")) {
+            //split masks
+            const [start, ...masks] = entry.trim().split("/");
+            
+            for (const mask of masks) {
+                data.push(
+                    fetch(
+                        baseURL + start + "/" + mask + "?" +
+                        (binarySw.checked ? "binary=true" : "binary=false")
+                    ).then(
+                        response => {return response.json()}
+                    ).then(
+                        x => data.push(x)
+                    )
+                );
+            }
+        }
+
+        console.log(data);
     } else {
         outputDiv.innerHTML = "<h3>No input data</h3>";
     }
