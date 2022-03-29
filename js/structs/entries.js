f82faf89af90642316620c7c39366c64a58db99d
Davide <davide.dimarco@marconirovereto.it>
2022-03-29 22:04:14 +0200

    create file and add AddressField class for binary and numerical fields in Subnet class, add Subnet class to represent a single part of a query, add entryList to represent a list of Subnets

---
+++ js/structs/entries.js
@@ -0,0 +1,56 @@
+export class entryList {
+    // TODO: complete array struct
+}
+
+export class AddressField {
+    // masks data
+    wildcardMask;
+    subnetMask;
+
+    // addresses
+    firstAssignableHost;
+    lastAssignableHost;
+    broadcastAddress;
+    networkAddress;
+    cidr;
+
+    constructor (
+        wildcardMask,
+        subnetMask,
+        firstAssignableHost,
+        lastAssignableHost,
+        broadcastAddress,
+        networkAddress,
+        cidr
+    ) {
+        this.wildcardMask = wildcardMask;
+        this.subnetMask   = subnetMask;
+        
+        this.firstAssignableHost = firstAssignableHost;
+        this.lastAssignableHost  = lastAssignableHost;
+        this.broadcastAddress    = broadcastAddress;
+        this.networkAddress      = networkAddress;
+        this.cidr                = cidr;
+    }
+}
+
+export class Subnet {
+    assignableHosts;
+    subnetMaskBits;
+    
+    numerical; // type AddressField
+    binary;    // type AddressField
+
+    constructor (numericalFields, binaryFields, mask) {
+        if (!(numericalFields instanceof AddressField))  throw new Error(`invalid numerical field ${numericalFields}`);
+        if (!(binaryFields    instanceof AddressField))  throw new Error(`invalid binary field ${binaryFields}`);
+        if (!mask | isNaN(mask) | mask > 32 | mask <= 0) throw new Error(`invalid mask input: ${mask}`);
+
+        this.subnetMaskBits  = mask;
+        this.assignableHosts = Math.pow(2, 32 - mask) - 2;
+        
+        this.numerical = numericalFields;
+        this.binary    = binaryFields;
+    }
+}
+
